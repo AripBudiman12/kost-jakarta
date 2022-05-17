ad73c27a3f233b3b3b4f1d0e0c8386d5aba1bcf1
AripBudiman12 <arip24.ab@gmail.com>
2022-05-17 12:31:13 +0700

    configurasi folder config autoload config dan database

---
+++ application/views/home/header_h.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Kost-Jakarta</title>
+
+    <style>
+        .jumbotron{
+
+            background-image: linear-gradient(to bottom, rgba(5,8,6,0.6) 0%,rgba(0,0,0,0.6) 100%),url(<?php echo base_url() ?>assets/iamge/bg,jpg);
+        }
+    </style>
+</head>
+<body>
