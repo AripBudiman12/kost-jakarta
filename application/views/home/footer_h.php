ef60eb485abc2d0ffcdc6203da402df1f3b8f612
AripBudiman12 <arip24.ab@gmail.com>
2022-05-23 01:10:09 +0700

    membuat navbar menjadi transparant

--- application/views/home/footer_h.php
+++ application/views/home/footer_h.php
@@ -1,6 +1,6 @@
 <!-- Main Footer -->
-<footer class="text-center">
-    <strong>Copyright &copy; KOST-JAKRTA <a href="<?= base_url('home'); ?>">WEB II UNIVERSITAS BINASARANA INVORMATIKA</a>.</strong>
+<footer class="text-center" style="background-color:gray">
+    <strong>Copyright &copy; KOST-JAKRTA <a href="<?= base_url('home'); ?>" style="color:orange">WEB II UNIVERSITAS BINASARANA INFORMATIKA</a>.</strong>
 </footer>
 </div>
 <!-- ./wrapper -->
--- application/views/home/isi_h.php
+++ application/views/home/isi_h.php
@@ -2,6 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container-fluid">      
         <span class="navbar-brand">KOST-JAKARTA</span>
+            
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -25,10 +26,24 @@
 <!-- jumbptron -->
 <div class="jumbotron">
     <div class="container">
+        <div class="jb">
+        <div id="carouselExampleInterval" class="carousel slide mx-auto" data-ride="carousel" >
+                  <div class="carousel-inner" >
+                      <div class="carousel-item active" >
+                      <img src="<?php echo base_url() ?>assets/image/iklan/iklan.jpg" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item" >
+                      <img src="<?php echo base_url() ?>assets/image/iklan/iklan2.png" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                      <img src="<?php echo base_url() ?>assets/image/iklan/iklan3.jpg" class="d-block w-100" alt="...">
+                      </div>
+                  </div>
+               </div>
 
-        <h3 class="display-4 text-center my-sm-3">Selamat datang di <b>KOST-JAKARTA</b></h3>
-        <h2 class="text-center">Kost di DKI JAKARTA</h2>
-        <h4 class="text-center" style="color:white">cari berdasarkan <b style="color:orange">Lokasi dan Harga</h4>
-
+            <h3 class="display-4 text-center my-sm-3">Selamat datang di <b style="color:orange">KOST-JAKARTA</b></h3>
+            <h2 class="text-center">Kost di DKI JAKARTA</h2>
+            <h4 class="text-center" style="color:white">cari berdasarkan <b style="color:orange">Lokasi dan Harga</b></h4>
+        </div>
     </div>
 </div>
