048ac9d532c44c9b5649fe172acc868c38edf34e
AripBudiman12 <arip24.ab@gmail.com>
2022-05-31 23:06:43 +0700

    mengubah nama folder imange menjadi img pada assets dan menambah function ubahKost pada controllers/kost

--- application/controllers/Kost.php
+++ application/controllers/Kost.php
@@ -15,7 +15,6 @@ class Kost extends CI_Controller
         $data['judul'] = 'Data Kost';
         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
         $data['data-kost'] = $this->ModelKost->getKost()->result_array();
-        $data['buku-tamu'] = $this->BukuTamu->getTamu()->result_array();
 
         $this->form_validation->set_rules('lokasi', 'Lokasi', 'required|min_length[3]',[
             'required' => 'Lokasi Harus Diisi',
@@ -80,4 +79,65 @@ class Kost extends CI_Controller
         redirect('kost');
     }
 
+    public function ubahKost()
+    {
+        $data['judul'] = 'Ubah Data Kost';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $data['data-kost'] = $this->ModelKost->kostWhere(['id' => $this->uri->segment(3)])->result_array();
+
+        $this->form_validation->set_rules('lokasi', 'Lokasi', 'required|min_length[3]',[
+            'required' => 'Lokasi Harus Diisi',
+            'min_length' => 'Lokasi terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required|min_length[3]',[
+            'required' => 'Alamat Harus Diisi',
+            'min_length' => 'Alamat terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('subalamat', 'SubAlamat', 'required|min_length[3]',[
+            'required' => 'SubAlamat Harus diisi',
+            'min_length' => 'SubAlamat Terlalu Pendek'
+        ]);
+        $this->form_validation->set_rules('harga', 'Harga', 'required|min_length[3]numeric',[
+            'required' => 'Harga harus diisi',
+            'min_length' => 'Harga Terlalu Pendek',
+            'numeric' => 'Yang anda masukkan bukan angka'
+        ]);
+
+        //konfogurasi sebelum gambar diupload
+
+        $config['upload_path'] = './assets/img/kost';
+        $config['allowed_types'] = 'jpg|png|jpeg';
+        $config['max_size'] = '3000';
+        $config['max_width'] = '1024';
+        $config['max_height'] = '1000';
+        $config['file_name'] = 'img' . time();
+
+        //memuat atau memanggil library upload
+        $this->load->library('upload', $config);
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('kost/index', $data);
+            $this->load->view('templates/footer');
+        } else {
+            if ($this->upload->do_upload('image')) {
+                $image = $this->upload->data();
+                unlink('assets/img/kost/' . $this->input->post('old_pict', TRUE));
+                $gambar = $image['file_name'];
+            } else {
+                $gambar = $this->input->post('old_pict', TRUE);
+            }
+            $data = [
+                'lokasi' => $this->input->post('lokasi', true),
+                'alamat' => $this->input->post('alamat', true),
+                'subalamat' => $this->input->post('subalamat', true),
+                'harga' => $this->input->post('harga', true),
+                'image' => $gambar
+            ];
+
+            $this->ModelKost->updateKost($data, ['id' => $this->input->post('id')]);
+            redirect('kost');
+        }
+    }
 }
