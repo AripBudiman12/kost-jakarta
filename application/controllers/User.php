d046658ab85d19ba67433b5d6a5b09224af5f884
AripBudiman12 <arip24.ab@gmail.com>
2022-06-01 22:43:12 +0700

    menambahkan file User Controller

---
+++ application/controllers/User.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') or exit('No direct script allowed');
+
+class User extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        cek_login();
+    }
+
+    public function index()
+    {
+        $data['judul'] = 'Profil Saya';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function anggota()
+    {
+        $data['judul'] = 'Data Anggota';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])
+        ->row_array();
+        $this->db->where('role_id', 1);
+        $data['anggota'] = $this->db->get('user')->result_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/anggota', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function ubahProfil()
+    {
+        $data['judul'] = 'Ubah Profil';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+
+        $this->form_validation->set_rules('nama', 'Nama Lengkap', 'required|trim', [
+            'required' => 'Nama Tidak boleh kosong'
+        ]);
+
+        
+    }
+}
