e8f16e54753553e003b5cd03cdb14751df71ffaa
AripBudiman12 <arip24.ab@gmail.com>
2022-06-01 10:36:26 +0700

    meminimalisir path pada header_h dan footer_h

--- application/views/home/footer_h.php
+++ application/views/home/footer_h.php
@@ -1,32 +1,10 @@
-<!-- Main Footer -->
-<footer class="text-center" style="background-color:gray">
-    <strong>Copyright &copy; KOST-JAKRTA <a href="<?= base_url('home'); ?>" style="color:orange">WEB II UNIVERSITAS BINASARANA INFORMATIKA</a>.</strong>
-</footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-<!-- jQuery -->
-<script src="assets/lte/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap -->
-<script src="assets/lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="assets/lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="assets/lte/dist/js/adminlte.js"></script>
-
-<!-- PAGE PLUGINS -->
-<!-- jQuery Mapael -->
-<script src="assets/lte/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-<script src="assets/lte/plugins/raphael/raphael.min.js"></script>
-<script src="assets/lte/plugins/jquery-mapael/jquery.mapael.min.js"></script>
-<script src="assets/lte/plugins/jquery-mapael/maps/usa_states.min.js"></script>
-<!-- ChartJS -->
-<script src="assets/lte/plugins/chart.js/Chart.min.js"></script>
-
-<!-- AdminLTE for demo purposes -->
-<script src="assets/lte/dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="assets/lte/dist/js/pages/dashboard2.js"></script>
+    <!-- Main Footer -->
+    <footer class="text-center" style="background-color:gray">
+        <strong>Copyright &copy; KOST-JAKRTA <a href="<?= base_url('home'); ?>" style="color:orange">WEB II UNIVERSITAS BINASARANA INFORMATIKA</a>.</strong>
+    </footer>
+    <!-- jQuery -->
+    <script src="<?= base_url('assets/'); ?>lte/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?= base_url('assets/'); ?>/lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- application/views/home/header_h.php
+++ application/views/home/header_h.php
@@ -14,15 +14,9 @@
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="assets/lte/plugins/fontawesome-free/css/all.min.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="assets/lte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="assets/lte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="assets/lte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="assets/lte/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/'); ?>lte/dist/css/adminlte.min.css">
   <!-- css -->
-  <link rel="stylesheet" href="assets/frontend.css">
+  <link rel="stylesheet" href="<?= base_url('assets/'); ?>frontend.css">
 </head>
 <body>
