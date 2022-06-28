92547c750a64ceb29ba2b9883b25e9f9bff1d9ba
nxrmalaa <arip24.ab@gmail.com>
2022-06-28 12:38:14 +0700

    modif dikit

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,65 +1,91 @@
-    <!-- Index -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0">Kost Jakarta</h1>
-          </div>
+          </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active"><?= $judul; ?></li>
             </ol>
-          </div>
-        </div>
-      </div>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
     </div>
-    
+    <!-- /.content-header -->
+
+    <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
+            <!-- Info boxes -->
             <div class="row">
                 <div class="col-12 col-sm-6 col-md-3">
                     <div class="info-box">
-                        <span class="info-box-icon bg-info elevation-1"><i class="fas fa-building"></i></span>
+                    <span class="info-box-icon bg-info elevation-1"><i class="fas fa-building"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">KOST</span>
                             <span class="info-box-number">
                                 <?= $this->ModelKost->kostWhere(['id'])->num_rows(); ?>
                             </span>
                         </div>
+                    <!-- /.info-box-content -->
                     </div>
+                    <!-- /.info-box -->
                 </div>
+                <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-3">
                     <div class="info-box mb-3">
-                        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-city"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">PROVINSI</span>
-                            <span class="info-box-number">5</span>
-                        </div>
+                    <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-city"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">PROVINSI</span>
+                        <span class="info-box-number">5</span>
                     </div>
+                    <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
                 </div>
-                
+                <!-- /.col -->
+
+                <!-- fix for small devices only -->
+                <div class="clearfix hidden-md-up"></div>
+
                 <div class="col-12 col-sm-6 col-md-3">
                     <div class="info-box mb-3">
-                        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-image"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">IMAGE</span>
-                            <span class="info-box-number"><?= $this->ModelKost->kostWhere(['id'])->num_rows(); ?></span>
-                        </div>
+                    <span class="info-box-icon bg-success elevation-1"><i class="fas fa-image"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">IMAGE</span>
+                        <span class="info-box-number">
+                            <?= $this->ModelKost->kostWhere(['id'])->num_rows(); ?>
+                        </span>
                     </div>
+                    <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
                 </div>
+                <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-3">
                     <div class="info-box mb-3">
                         <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>        
                         <div class="info-box-content">
                             <span class="info-box-text">USER</span>
-                            <span class="info-box-number"><?= $this->ModelUser->getUserWhere()->num_rows(); ?></span>
+                            <span class="info-box-number">
+                                <?= $this->ModelUser->getUserWhere()->num_rows(); ?>
+                            </span>
                         </div>
+                        <!-- /.info-box-content -->
                     </div>
+                    <!-- /.info-box -->
                 </div>
+                <!-- /.col -->
             </div>
+            <!-- /.row -->
             <div class="row">
                 <div class="col-md-6">
+                        <!-- USERS LIST -->
                         <div class="card">
                             <div class="card-header">
                                 <h3 class="card-title">Daftar User</h3>
@@ -72,27 +98,34 @@
                                     </button>
                                 </div>
                             </div>
+                            <!-- /.card-header -->
                             <div class="card-body p-0">
                                 <ul class="users-list clearfix">
                                     <?php
                                     foreach ($data_user as $a) { ?>
                                         <li>
                                             <picture>
-                                                <source srcset="" type="image/svg+xml">
-                                                <img src="<?= base_url('assets/img/user/') . $a['image']; ?>" class="img-fluid img-thumbnail" alt="User Image" style="width:60px;height:80px;">
-                                            </picture>
+                                            <source srcset="" type="image/svg+xml">
+                                            <img src="<?= base_url('assets/img/user/') . $a['image']; ?>" class="img-fluid img-thumbnail" alt="User Image" style="width:60px;height:80px;">
+                                        </picture>
                                             <a class="users-list-name" href="#"><?= $a['nama']; ?></a>
                                             <span class="users-list-date"><?= date('d F Y', $a['tanggal_input']); ?></span>
                                         </li>
                                     <?php } ?>
                                 </ul>
+                                <!-- /.users-list -->
                             </div>
+                            <!-- /.card-body -->
                             <div class="card-footer text-center">
                                 <a href="<?= base_url('User/anggota'); ?>">View All Users</a>
                             </div>
+                            <!-- /.card-footer -->
                         </div>  
+                        <!--/.card -->
                     </div>
-                    
+                    <!-- col -->
+
+                     <!-- PRODUCT LIST -->
                     <div class="card">
                         <div class="card-header">
                             <h3 class="card-title">Recently Added Products</h3>
@@ -106,6 +139,7 @@
                                 </button>
                             </div>
                         </div>
+                        <!-- /.card-header -->
                         
                         <div class="card-body p-0">
                             <ul class="products-list product-list-in-card pl-2 pr-2">
@@ -131,25 +165,32 @@
                                 <?php } ?>
                             </ul>
                         </div>
+                        <!-- /.card-body -->
                         <div class="card-footer text-center">
                             <a href="<?= base_url('Kost'); ?>" class="uppercase">View All Products</a>
                         </div>
+                        <!-- /.card-footer -->
                     </div>
+                    <!-- /.card -->
                 </div>
+                        <!-- /.col -->
                 </div>
+                <!-- /.row -->
+                <!-- TABLE: LATEST ORDERS -->
                 <div class="card">
-                    <div class="card-header border-transparent">
-                        <h3 class="card-title">Buku Tamu</h3>
-                        <div class="card-tools">
-                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                                <i class="fas fa-minus"></i>
-                            </button>
-                            <button type="button" class="btn btn-tool" data-card-widget="remove">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
+                <div class="card-header border-transparent">
+                    <h3 class="card-title">Buku Tamu</h3>
+
+                    <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                    </button>
                     </div>
-                    <!-- /.card-header -->
+                </div>
+                <!-- /.card-header -->
                 <div class="card-body p-0">
                     <div class="table-responsive">
                     <table class="table m-0">
@@ -177,13 +218,20 @@
                         </tbody>
                     </table>
                     </div>
+                    <!-- /.table-responsive -->
                 </div>
+                <!-- /.card-body -->
                 <div class="card-footer clearfix">
                     <a href="<?= base_url('Buku_Tamu/tamu'); ?>" class="btn btn-sm btn-info float-right">View All Coment</a>
                 </div>
+                <!-- /.card-footer -->
+                </div>
+                <!-- /.card -->
             </div>
+            <!-- /.col -->    
+        </div>
+        <!--/. container-fluid -->
     </section>
-
-    <aside class="control-sidebar control-sidebar-dark">
-
-    </aside>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
